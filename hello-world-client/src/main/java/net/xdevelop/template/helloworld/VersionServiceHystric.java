79497ef4be755bdad668800225f061665445208b
kmxforce <8707115@qq.com>
2019-08-30 20:51:09 +0800

    update

---
+++ hello-world-client/src/main/java/net/xdevelop/template/helloworld/VersionServiceHystric.java
@@ -0,0 +1,11 @@
+package main.java.net.xdevelop.template.helloworld;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class VersionServiceHystric implements IVersionService {
+    @Override
+    public String getVersion() {
+        return "unknown version";
+    }
+}
